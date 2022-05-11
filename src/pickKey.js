26fb8cc2469e002fb5e12b4c7daa25109f1cfa21
Maria Kagermanova <maria.kgm04@gmail.com>
2022-05-11 21:35:31 +0300

    feat: fix backspace

--- src/pickKey.js
+++ src/pickKey.js
@@ -1,7 +1,7 @@
 let nowWatch = ['Delete','Shift','Win','Alt','Ctrl','CapsLock'];
 let start;
 export default function pickKey(text, key) {
-  start = text.value.length-1;
+  start = text.value.length;
   if (text.selectionStart !== start) {
     start = text.selectionStart;
   }
@@ -11,10 +11,10 @@ export default function pickKey(text, key) {
     text.setSelectionRange(start + 1, start + 1);
   }, 50);
   if (nowWatch.includes(key)) start--;
-  else if (key == 'Enter') text.value = text.value.slice(0, start) + '\n' + text.value.slice(start, text.value.length);
   else if (key == 'Backspace') {
-    if (text.selectionStart == text.selectionEnd) {
-      text.value = text.value.slice(0, start-1) + text.value.slice(start, text.value.length);
+    if (text.selectionStart == 0 && text.selectionEnd == 0) start--;
+    else if (text.selectionStart == text.selectionEnd) {
+      text.value = text.value.slice(0, text.selectionStart - 1) + text.value.slice(text.selectionEnd, text.value.length);
       start -= 2;
     }
     else {
@@ -22,6 +22,7 @@ export default function pickKey(text, key) {
       start--;
     }
   }
+  else if (key == 'Enter') text.value = text.value.slice(0, start) + '\n' + text.value.slice(start, text.value.length);
   else if (key == 'Del') {
     text.value = text.value.slice(0, start) + text.value.slice(start + 1, text.value.length);
     start--;
@@ -31,5 +32,7 @@ export default function pickKey(text, key) {
     start += 2;
   }
   else if (key == '') text.value = text.value.slice(0, start) + ' ' + text.value.slice(start, text.value.length);
-  else text.value = text.value.slice(0, start) + key + text.value.slice(start, text.value.length);
+  else {
+    text.value = text.value.slice(0, text.selectionStart) + key + text.value.slice(text.selectionEnd, text.value.length);
+  }
 }
